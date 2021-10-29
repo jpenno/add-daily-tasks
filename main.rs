a67547b9cb7174826f9802414dc78b40044b4b86
jp <jpenno123@hotmail.com>
2021-10-29 19:22:27 +1100

    read from file

--- main.rs
+++ main.rs
@@ -1,3 +1,13 @@
+// use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // --snip--
+    let filename = String::from("tasks.org");
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
